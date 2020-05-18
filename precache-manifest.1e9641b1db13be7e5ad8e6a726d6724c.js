fcc6517e5f5f47d37f2fcdda0d66e55165524350
paszo <paszo2008@gmail.com>
2020-05-18 18:12:28 +0200

    Updates

---
+++ precache-manifest.1e9641b1db13be7e5ad8e6a726d6724c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58b85634086f47af1329e68d1f8d84c7",
+    "url": "/paszo/second-react/index.html"
+  },
+  {
+    "revision": "f03ee2f305f69603a8b6",
+    "url": "/paszo/second-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "68a1c262ed99b05b185a",
+    "url": "/paszo/second-react/static/js/2.5b6f9b2e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/paszo/second-react/static/js/2.5b6f9b2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f03ee2f305f69603a8b6",
+    "url": "/paszo/second-react/static/js/main.b3283f65.chunk.js"
+  },
+  {
+    "revision": "292a13764ef354873aa7",
+    "url": "/paszo/second-react/static/js/runtime-main.b5f8496e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/paszo/second-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
